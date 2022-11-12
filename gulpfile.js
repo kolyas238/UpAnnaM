35849f2e5c5382732f2cdc5965ee8acccabdf6b5
kolyas238 <kolyas238@gmail.com>
2022-11-12 17:22:02 +0300

    team section styles added

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,8 @@ gulp.task("server", function () {
   server.init({
     server: "build/",
     notify: false,
-    online: true,
-    tunnel: "nft-bored-anna",
+    // online: true,
+    // tunnel: "nft-bored-anna",
     open: "internal",
     cors: true,
     ui: false
